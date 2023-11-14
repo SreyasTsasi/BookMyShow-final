2dc3a7f0dd4532edbdd3b05f9a69782f9372294d
sreyas t sasi <71540653+SreyasTsasi@users.noreply.github.com>
2023-11-14 13:04:01 +0530

    Add files via upload

---
+++ File.js
@@ -0,0 +1,7 @@
+
+var a=document.getElementById("imageChange");
+var b=document.getElementById("imageToChange");
+
+a.addEventListener("click", function() {
+  b.setAttribute("src","assets/open2.jpg")
+});
